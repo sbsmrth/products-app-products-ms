08fccc008b4ba15c1898c606bf83fe6da05a6182
sbsmrth <andresf.mrh@gmail.com>
2025-07-21 17:38:33 -0500

    feat: configuration for product microservice

---
+++ src/config/envs.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  DATABASE_URL: string;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
+  })
+  .unknown(true); // Allow unknown properties
+
+const { error, value } = envsSchema.validate(process.env) as {
+  error: joi.ValidationError;
+  value: EnvVars;
+};
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  databaseUrl: envVars.DATABASE_URL,
+};
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port,
+    },
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+  logger.log(`PRODUCTS Microservice is running on port ${envs.port}`);
+}
+bootstrap();
